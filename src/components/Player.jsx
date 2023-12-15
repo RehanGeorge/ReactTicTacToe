55c334ff28a7bbd29401ca66386a4f14bc0f5bdf
Rehan George <rehan.george@gmail.com>
2023-12-16 01:25:36 +0530

    Add header and player components, and implement game board functionality

---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [editing, setEditing] = useState(false);
+
+    function handleEditClick() {
+        setEditing((editing) => !editing);
+    }
+
+    function handlePlayerNameChange(event) {
+        setPlayerName((playerName) => event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (editing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handlePlayerNameChange} />;
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName }
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{editing ? 'Save': 'Edit'}</button>
+        </li>
+    )
+}
